82081879b521a492df65c555a2a82973d8983762
ahmetpalavan <ahmetpalavann@gmail.com>
2022-09-28 02:31:46 +0300

    project finished

--- chat-app/src/components/Message.js
+++ chat-app/src/components/Message.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 
-const Message = () => {
+const Message = ({message}) => {
+    const {currentUser}=useContext(AuthContext)
+    const {data} = useContext(ChatContext)
+    const ref = useRef()
+    // console.log(message);
+    useEffect(()=>{
+        ref.current?.scrollIntoView({behavior:'smooth'})
+    },[message])
     return (
-        <div className='message owner'>
+        <div ref={ref} className={`message ${message.senderId === currentUser.uid && 'owner'}`}>
             <div className="messageInfo">
-                <img src="https://images.pexels.com/photos/13623557/pexels-photo-13623557.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                <img src={message.senderId===currentUser.uid
+                ? currentUser.photoURL
+                : data.user.photoURL
+                } alt="" />
                 <span>Just now</span>
             </div>
             <div className="messageContent">
-                <p>Hello</p>
-                <img src="https://images.pexels.com/photos/13623557/pexels-photo-13623557.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                <p>{message.text}</p>
+                {message.img && <img src={message.img} alt="" />}
             </div>
         </div>
     )
---
+++ chat-app/src/context/ChatContext.js
@@ -0,0 +1,33 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
+import { auth } from "../firebase";
+import { AuthContext } from "./AuthContext";
+
+export const ChatContext=createContext();
+// eslint-disable-next-line react-hooks/rules-of-hooks
+export const ChatContextProvider = ({children}) => {
+    const {currentUser}=useContext(AuthContext)
+    const INITIAL_STATE={
+        chatId:'null',
+        user:{}
+    }
+    const chatReducer=(state,action)=>{
+        switch (action.type) {
+            case 'CHANGE_USER':
+            return{
+                user:action.payload,
+                chatId:currentUser.uid>action.payload.uid
+                ? currentUser.uid + action.payload.uid 
+                : action.payload.uid + currentUser.uid
+            }
+                default:
+                return state 
+        }
+    }
+    const [state,dispatch]=useReducer(chatReducer,INITIAL_STATE)
+    return(
+    <ChatContext.Provider value={{data:state,dispatch}}>
+        {children}
+    </ChatContext.Provider>
+    )
+}
